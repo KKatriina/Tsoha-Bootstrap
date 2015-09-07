4e16a49adb95b62627c5147a886c23712bbe58ad
Kerokoski Heini K <kkerokos@da5-cs-bk107-16.helsinki.fi>
2015-09-07 16:00:39 +0300

    html-sivujen aloitus

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,35 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+      View::make('suunnitelmat/etusivu.html');
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
+    }
+    
+    public static function etusivu() {
+        View::make('suunnitelmat/etusivu.html');
+    }
+    
+    public static function kurssit() {
+        View::make('suunnitelmat/kurssit.html');
+    }
+    
+    public static function kyselyt() {
+        View::make('suunnitelmat/kyselyt.html');
+    }
+    
+    public static function muokkaa_kurssia() {
+        View::make('suunnitelmat/muokkaa_kurssia.html');
+    }
+    
+    public static function kurssin_esittelysivu() {
+        View::make('suunnitelmat/kurssin_esittelysivu.html');
+    }
+    
+    public static function tervetuloa() {
+        View::make('suunnitelmat/tervetuloa.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,28 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/etusivu', function() {
+  HelloWorldController::etusivu();
+  });
+  
+  $routes->get('/kurssit', function() {
+  HelloWorldController::kurssit();
+  });
+  
+  $routes->get('/kyselyt', function() {
+  HelloWorldController::kyselyt();
+  });
+  
+  $routes->get('/muokkaa_kurssia', function() {
+  HelloWorldController::muokkaa_kurssia();
+  });
+  
+  $routes->get('/kurssin_esittelysivu', function() {
+  HelloWorldController::kurssin_esittelysivu();
+  });
+  
+  $routes->get('/tervetuloa', function() {
+  HelloWorldController::tervetuloa();
+  });
+  
