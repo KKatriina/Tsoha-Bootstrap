6fd9888e1952f203fe11547dd9c3feb2e0d25f8f
Kerokoski Heini K <kkerokos@da5-cs-b221-07.helsinki.fi>
2015-09-09 15:51:15 +0300

    tietokantataulut pystytetty

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,2 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+--INSERT INTO Opettaja(nimi, salasana, laitos, nimike) VALUES ('Setämies', 'abc123', 'Matematiikan ja tilastotieteen laitos', 'professori');
+INSERT INTO Kurssi(nimi, aika, tyyppi, kuvaus, opettaja) VALUES ('Raja-arvot', 'I periodi', 1, 'Hämmennyksen kautta helpotukseen', 'Setämies');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,34 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Opettaja(
+    nimi varchar(50) PRIMARY KEY,
+    salasana varchar(50) NOT NULL,
+    laitos varchar(50),
+    nimike varchar(50)
+);
+
+CREATE TABLE Kysely(
+    nimi varchar(50) PRIMARY KEY,
+    tarkoitus varchar(50)
+);
+
+CREATE TABLE Kysymys(
+    tunniste SERIAL PRIMARY KEY,
+    kysely varchar(50) REFERENCES Kysely(nimi),
+    kysymys varchar(400)
+);
+
+CREATE TABLE Kurssi(
+    tunniste SERIAL PRIMARY KEY,
+    nimi varchar(50),
+    aika varchar(50),
+    tyyppi INTEGER,
+    kuvaus varchar(400),
+    opettaja varchar(50) REFERENCES Opettaja(nimi)
+);
+
+CREATE TABLE Kurssin_kysely(
+    tunniste SERIAL PRIMARY KEY,
+    kurssi INTEGER REFERENCES Kurssi(tunniste),
+    kysely varchar(50) REFERENCES Kysely(nimi),
+    paattyminen DATE
+);
+
