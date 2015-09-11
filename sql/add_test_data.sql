8ce4850210d0dfeaebbbc28b3d6cbe4acc3424a6
Kerokoski Heini K <kkerokos@da5-cs-bk107-04.helsinki.fi>
2015-09-11 13:27:51 +0300

    yksi esimerkkirivi

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,2 +1,2 @@
---INSERT INTO Opettaja(nimi, salasana, laitos, nimike) VALUES ('Setämies', 'abc123', 'Matematiikan ja tilastotieteen laitos', 'professori');
+INSERT INTO Opettaja(nimi, salasana, laitos, nimike) VALUES ('Setämies', 'abc123', 'Matematiikan ja tilastotieteen laitos', 'professori');
 INSERT INTO Kurssi(nimi, aika, tyyppi, kuvaus, opettaja) VALUES ('Raja-arvot', 'I periodi', 1, 'Hämmennyksen kautta helpotukseen', 'Setämies');
